fb713f1e6f9c7993bb4e86d9b739b060495e0583
immortalua96 <romanshvagla@icloud.com>
2023-06-04 15:19:38 +0200

    start

--- src/index.js
+++ src/index.js
@@ -11,11 +11,10 @@ const DEBOUNCE_DELAY = 300;
 input.addEventListener(
   'input',
   debounce(e => {
-      const trimmedValue = input.value.trim();
-         cleanHtml();   
+    const trimmedValue = input.value.trim();
+    cleanHtml();
     if (trimmedValue !== '') {
-        fetchCountries(trimmedValue).then(foundData => {      
-
+      fetchCountries(trimmedValue).then(foundData => {
         if (foundData.length > 10) {
           Notiflix.Notify.info(
             'Too many matches found. Please enter a more specific name.'
@@ -23,11 +22,8 @@ input.addEventListener(
         } else if (foundData.length === 0) {
           Notiflix.Notify.failure('Oops, there is no country with that name');
         } else if (foundData.length >= 2 && foundData.length <= 10) {
-         
           renderCountryList(foundData);
-          
         } else if (foundData.length === 1) {
-    
           renderOneCountry(foundData);
         }
       });
@@ -39,38 +35,28 @@ function renderCountryList(countries) {
   const markup = countries
     .map(country => {
       return `<li class="item-cantry">
-      <img src="${country.flags.svg}" alt="Flag of ${
-        country.name.official
-      }" width="30" hight="20">
+      <img src="${country.flags.svg}" alt="Flag of ${country.name.official}" width="30" hight="20">
          <b>${country.name.official}</b>
                 </li>`;
     })
     .join('');
-  countryList.innerHTML = markup;
-  
- 
-  
 }
 
-
-  
-
-
 function renderOneCountry(countries) {
-      const markup = countries
-        .map(country => {
-          return `<li>
+  const markup = countries
+    .map(country => {
+      return `<li>
       <img src="${country.flags.svg}" alt="Flag of ${
-            country.name.official
-          }" width="30" hight="20">
+        country.name.official
+      }" width="30" hight="20">
          <b>${country.name.official}</b></p>
             <p><b>Capital</b>: ${country.capital}</p>
             <p><b>Population</b>: ${country.population}</p>
             <p><b>Languages</b>: ${Object.values(country.languages)} </p>
                 </li>`;
-        })
-        .join('');
-      countryList.innerHTML = markup;
+    })
+    .join('');
+  countryList.innerHTML = markup;
 }
 
 function cleanHtml() {
